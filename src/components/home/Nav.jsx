1dc0718e77466f05d1d55d25d90d0763e0906a9a
piojeet <manjhipiojeet@gmail.com>
2025-08-11 15:03:27 +0530

    first commit

---
+++ src/components/home/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Logo from '../../assets/logo.png'
+
+function Nav() {
+  return (
+    <div>
+        <div className='max-w-[1440px] mx-auto flex justify-between items-center p-4 md:px-8 releative'>
+            <div><img src={Logo} alt="logo" className='w-[138px]' /></div>
+            <div className='flex itcems-center gap-4'>
+                <button className='bg-accent text-light py-2 md:px-8 px-4 rounded-full'>The Job Journal</button>
+                <button className='bg-accent text-light py-2 md:px-8 px-4 rounded-full'>Job Jarvis</button>
+            </div>
+
+            <div className='absolute flex items-center left-1/2 transform -translate-x-1/2 text-5xl font-bold flex items-center gap-2'>
+                <span className='text-secondary'>Infinity</span>
+                <span><img src={Logo} alt="logo" className='w-[70px]' /></span>
+                <span>Beyond</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF', // blue
+        secondary: '#7A1C1C', // amber
+        accent: '#000000', // emerald
+        dark: '#1A1A1D', // gray-900
+        light: '#FFF8F1', // gray-100
+      },
+    },
+  },
+  plugins: [],
+}
