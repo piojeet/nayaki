98822eed7f78f5c3d3812875b34b21abf899d407
piojeet <manjhipiojeet@gmail.com>
2025-08-12 12:24:05 +0530

    message

--- src/components/home/HeroLeft.jsx
+++ src/components/home/HeroLeft.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import HeroRight from './HeroRight'
+import Spline from '@splinetool/react-spline';
 
 function HeroLeft() {
   return (
     <div>
-        <div className='max-w-[1440px] mx-auto flex flex-col md:flex-row items-center justify-between p-4 md:px-8'>
-            <div></div>
-            <HeroRight />
+      <div className='max-w-[1440px] mx-auto flex flex-col md:flex-row items-center justify-between p-4 md:px-8'>
+        <div className='w-1/2 h-[40vh]'>
+          <Spline scene="https://prod.spline.design/i56Sll5KbkCWmTPy/scene.splinecode" />
         </div>
+        <HeroRight />
+      </div>
     </div>
   )
 }
